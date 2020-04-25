7c03d006bea159eef1d5e1f50baba3f9a32b45cf
Dirk <dbeukman.delia@beukman.co.za>
2020-04-25 17:58:32 +0200

    Added About.php

---
+++ About.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="CSS/Layout.css" rel="stylesheet" type="text/css" />
+<link href="CSS/Menu.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>BMTC</title>
+</head>
+
+<body>
+<div id="Holder">
+<div id="Header"></div>
+<div id="NavBar">
+	<nav>
+    	<ul>
+        	<li><a href="index.php">Home</a></li>
+        </ul>
+    </nav>
+</div>
+<div id="Content">
+	<div id="PageHeading">
+	  <h1>H.O.D. TEAM</h1>
+	</div>
+	<div id="ContentLeft">
+	  <h2><strong>BMTC</strong></h2>
+    <strong>Is an Accredited Training Facility, with extraordinary ETD Practitioners. We strive to be the Worlds best Training Centre in order to Educate, Train and Develop all Employees who train at our World Class Facility. Here the Trainees needs comes first and we are professional in what we do!</strong></div>
+    <div id="ContentRight"></div>
+</div>
+<div id="Footer"></div>
+<form id="form1" name="form1" method="post" action="">
+  <table width="500" border="0" align="center">
+    <tr>
+      <td align="center">Mauritz Botha</td>
+      <td align="center">Dirk Beukman</td>
+      <td align="center">Martin Maré</td>
+      <td align="center">Abram Tladi</td>
+      <td align="center">Dave Fourie</td>
+      <td align="center">Esmé Atherstone</td>
+    </tr>
+    <tr>
+      <td><img src="assets/Dirk.JPG" alt="" width="150" height="150" /></td>
+      <td><img src="assets/Dirk.JPG" alt="" width="150" height="150" /></td>
+      <td><img src="assets/Dirk.JPG" alt="" width="150" height="150" /></td>
+      <td><img src="assets/Dirk.JPG" alt="" width="150" height="150" /></td>
+      <td><img src="assets/Dirk.JPG" alt="" width="150" height="150" /></td>
+      <td><img src="assets/Dirk.JPG" alt="" width="150" height="150" /></td>
+    </tr>
+    <tr>
+      <td align="center">HRD Manager</td>
+      <td align="center">HRD Co-Ordinator</td>
+      <td align="center">HRD Co-Ordinator</td>
+      <td align="center">HRD Co-Ordinator</td>
+      <td align="center">HRD Co-Ordinator</td>
+      <td align="center">HRD Co-Ordinator</td>
+    </tr>
+    <tr>
+      <td align="center">&nbsp;</td>
+      <td align="center">(Mine Overseer)</td>
+      <td align="center">(Quality Assurance)</td>
+      <td align="center">(Engineering)</td>
+      <td align="center">(Continuous Evaluation)</td>
+      <td align="center">(Admin)</td>
+    </tr>
+    <tr>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+      <td align="center">&nbsp;</td>
+    </tr>
+    <tr>
+      <td align="left">To add text</td>
+      <td align="left">23 Years experience in the Minding Industry. Experience in Conventional and Trackless Mines. Mine Overseers Certificate / NQF Level 4 / Assessors / Moderators / Ventilation Elementary Certificate / Radiation Screening Certificate / 21 Years Proto Experience.</td>
+      <td align="left">To add text</td>
+      <td align="left">To add text</td>
+      <td align="left">To add text</td>
+      <td align="left">To add text</td>
+    </tr>
+  </table>
+</form>
+</div>
+</body>
+</html>
+
