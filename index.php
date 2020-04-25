3a37018536444870e59295f3c175a8ac6b111d6c
Dirk <dbeukman.delia@beukman.co.za>
2020-04-25 18:04:22 +0200

    Added index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="CSS/Layout.css" rel="stylesheet" type="text/css" />
+<link href="CSS/Menu.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryValidationConfirm.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+body {
+	background-color: #FFF;
+}
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width" />
+<title>BMTC</title>
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<script src="SpryAssets/SpryValidationConfirm.js" type="text/javascript"></script>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+</form>
+<div id="Holder">
+<div id="Header"></div>
+<div id="NavBar">
+	<nav>
+    	<ul>
+            <li><a href="index.php">Home</a></li>
+        	<li><a href="Login.php">Login</a></li>
+            <li><a href="Register.php">Register</a></li>
+            <li><a href="ForgotPassword.php">Forgot Password</a></li>
+            <li><a href="#"></a></li>
+            <li><a href="About.php">About</a></li>
+        </ul>
+    </nav>
+</div>
+<div id="Content">
+	<div id="PageHeading">
+	  <h1>Welcome to ...</h1>
+	</div>
+	<div id="ContentLeft">
+	  <h2><u>BMTC</u></h2>
+	  <br />
+	  <br />
+      The best Training Centre ever!
+      
+	</div>
+    <div id="ContentRight">
+    <h2></h2>
+    </div>
+</div>
+<div id="Footer"></div>
+
+<div class="box">
+    <center><img src="assets/XLP.jpg" width="500" height="291" /></center>
+</div>
+
+<div id="Content">
+	<div id="PageFooter">
+      <br />
+      <br />
+	  <h1><center>Bathopele Mechanised Training Centre
+	    
+	    
+	  </center></h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+	</div>
+</div>
+
+</body>
+</html>
+
