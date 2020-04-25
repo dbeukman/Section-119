495580f044ff8b9816686c4f83b8f56ec6939c9e
Dirk <dbeukman.delia@beukman.co.za>
2020-04-25 18:03:14 +0200

    Added Register.php

---
+++ Register.php
@@ -0,0 +1,274 @@
+<?php 
+
+require_once('Connections/localhost.php'); 
+?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+// *** Redirect if username exists
+$MM_flag="MM_insert";
+if (isset($_POST[$MM_flag])) {
+  $MM_dupKeyRedirect="Register.php";
+  $loginUsername = $_POST['UserName'];
+  $LoginRS__query = sprintf("SELECT UserName FROM `user` WHERE UserName=%s", GetSQLValueString($loginUsername, "text"));
+  mysql_select_db($database_localhost, $localhost);
+  $LoginRS=mysql_query($LoginRS__query, $localhost) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+
+  //if there is a row in the database, the username was found - can not add the requested username
+  if($loginFoundUser){
+    $MM_qsChar = "?";
+    //append the username to the redirect page
+    if (substr_count($MM_dupKeyRedirect,"?") >=1) $MM_qsChar = "&";
+    $MM_dupKeyRedirect = $MM_dupKeyRedirect . $MM_qsChar ."requsername=".$loginUsername;
+    header ("Location: $MM_dupKeyRedirect");
+    exit;
+  }
+}
+
+// *** Redirect if username exists
+$MM_flag="MM_insert";
+if (isset($_POST[$MM_flag])) {
+  $MM_dupKeyRedirect="Register.php";
+  $loginUsername = $_POST['UserName'];
+  $LoginRS__query = sprintf("SELECT UserName FROM `user` WHERE UserName=%s", GetSQLValueString($loginUsername, "text"));
+  mysql_select_db($database_localhost, $localhost);
+  $LoginRS=mysql_query($LoginRS__query, $localhost) or die(mysql_error());
+  $loginFoundUser = mysql_num_rows($LoginRS);
+
+  //if there is a row in the database, the username was found - can not add the requested username
+  if($loginFoundUser){
+    $MM_qsChar = "?";
+    //append the username to the redirect page
+    if (substr_count($MM_dupKeyRedirect,"?") >=1) $MM_qsChar = "&";
+    $MM_dupKeyRedirect = $MM_dupKeyRedirect . $MM_qsChar ."requsername=".$loginUsername."&UserExists=Y";
+    header ("Location: $MM_dupKeyRedirect");
+    exit;
+  }
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "RegisterForm")) {
+  $insertSQL = sprintf("INSERT INTO user (Fname, Lname, Email, UserName, Password) VALUES (%s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['Fname'], "text"),
+                       GetSQLValueString($_POST['Lname'], "text"),
+                       GetSQLValueString($_POST['Email'], "text"),
+                       GetSQLValueString($_POST['UserName'], "text"),
+                       GetSQLValueString($_POST['Password'], "text"));
+
+  mysql_select_db($database_localhost, $localhost);
+  $Result1 = mysql_query($insertSQL, $localhost) or die(mysql_error());
+
+  $insertGoTo = "Login.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "RegisterForm")) {
+  $insertSQL = sprintf("INSERT INTO ``user`` (Fname, Lname, Email, UserName, Password) VALUES (%s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['Fname'], "text"),
+                       GetSQLValueString($_POST['Lname'], "text"),
+                       GetSQLValueString($_POST['Email'], "text"),
+                       GetSQLValueString($_POST['UserName'], "text"),
+                       GetSQLValueString($_POST['Password'], "text"));
+
+  mysql_select_db($database_localhost, $localhost);
+  $Result1 = mysql_query($insertSQL, $localhost) or die(mysql_error());
+
+  $insertGoTo = "Login.php";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+
+mysql_select_db($database_localhost, $localhost);
+$query_Register = "SELECT * FROM `user`";
+$Register = mysql_query($query_Register, $localhost) or die(mysql_error());
+$row_Register = mysql_fetch_assoc($Register);
+$totalRows_Register = mysql_num_rows($Register);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="CSS/Layout.css" rel="stylesheet" type="text/css" />
+<link href="CSS/Menu.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryValidationTextField.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryValidationConfirm.css" rel="stylesheet" type="text/css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width" />
+<title>BMTC</title>
+<script src="SpryAssets/SpryValidationTextField.js" type="text/javascript"></script>
+<script src="SpryAssets/SpryValidationConfirm.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div id="Holder">
+<div id="Header"></div>
+<div id="NavBar">
+	<nav>
+    	<ul>
+            <li><a href="index.php">Home</a></li>
+        	<li><a href="Login.php">Login</a></li>
+            <li><a href="Register.php">Register</a></li>
+        </ul>
+    </nav>
+</div>
+<div id="Content">
+	<div id="PageHeading">
+	  <h1>Sign Up!</h1>
+	</div>
+	<div id="ContentLeft">
+	  <h2><u>BMTC</u></h2>
+	  <br />
+	  <br />
+      Please Register to be able to sign in to our Website.
+      
+	</div>
+    <div id="ContentRight">
+    
+ <?php
+ if($_GET['UserExists']=='Y'){
+	 echo "<span style='padding:10px; margin:10px; background:red; color:#FFF; font-weigth:bold;'>This username already exists, please select a different username.</span>";
+ }
+ ?>
+ 
+ 
+      <form id="RegisterForm" name="RegisterForm" method="POST" action="<?php echo $editFormAction; ?>">
+        <table width="400" border="0" align="center">
+          <tr>
+            <td><table border="0">
+              <tr>
+                <td><h6><span id="sprytextfield1">
+                  <label for="Fname"></label>
+                  First Name:<br />
+                  <br />
+                  <input name="Fname" type="text" class="StyleTxtField" id="FirstName" />
+                </span></h6>
+                  <span><span class="textfieldRequiredMsg">A value is required.</span></span></td>
+                <td><h6><span id="sprytextfield2">
+                  <label for="Lname"></label>
+                  Last Name:<br />
+                  <br />
+                  <input name="Lname" type="text" class="StyleTxtField" id="LastName" />
+                </span></h6>
+                  <span><span class="textfieldRequiredMsg">A value is required.</span></span></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><h6><span id="sprytextfield3">
+              <label for="Email"></label>
+              Email:<br />
+  <br />
+  <input name="Email" type="text" class="StyleTxtField" id="Email" />
+            </span></h6>
+            <span><span class="textfieldRequiredMsg">A value is required.</span><span class="textfieldInvalidFormatMsg">Invalid format.</span></span></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><h6><span id="sprytextfield4">
+              <label for="UserName"></label>
+              UserName:<br />
+              <br />
+              <input name="UserName" type="text" class="StyleTxtField" id="UserName" />
+            </span></h6>
+            <span><span class="textfieldRequiredMsg">A value is required.</span></span></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><table border="0">
+              <tr>
+                <td><h6><span id="sprytextfield5">
+                  <label for="Password"></label>
+                  Password:<br />
+                  <br />
+                  <input name="Password" type="text" class="StyleTxtField" id="Password" />
+                </span></h6>
+                  <span><span class="textfieldRequiredMsg">A value is required.</span></span></td>
+                <td><h6><span id="spryconfirm1">
+                  <label for="PasswordConfirm"></label>
+                  Confirm Password:<br />
+                  <br />
+                  <input name="PasswordConfirm" type="text" class="StyleTxtField" id="PasswordConfirm" />
+                </span></h6>
+                  <span><span class="confirmRequiredMsg">A value is required.</span><span class="confirmInvalidMsg">The values don't match.</span></span></td>
+              </tr>
+            </table></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td><input type="submit" name="RegisterButton" id="RegisterButton" value="Register" /></td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+          <tr>
+            <td>&nbsp;</td>
+          </tr>
+        </table>
+        <input type="hidden" name="MM_insert" value="RegisterForm" />
+      </form>
+    </div>
+</div>
+<div id="Footer"></div>
+</div>
+<script type="text/javascript">
+var sprytextfield1 = new Spry.Widget.ValidationTextField("sprytextfield1");
+var sprytextfield2 = new Spry.Widget.ValidationTextField("sprytextfield2");
+var sprytextfield3 = new Spry.Widget.ValidationTextField("sprytextfield3", "email");
+var sprytextfield4 = new Spry.Widget.ValidationTextField("sprytextfield4");
+var sprytextfield5 = new Spry.Widget.ValidationTextField("sprytextfield5");
+var spryconfirm1 = new Spry.Widget.ValidationConfirm("spryconfirm1", "Password");
+</script>
+</body>
+</html>
+<?php
+mysql_free_result($Register);
+?>
